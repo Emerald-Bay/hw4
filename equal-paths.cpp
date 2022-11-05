9cf15bf6a76398198772ca88e46c4ff86162e8e4
Jake Hosking <hosking@usc.edu>
2022-11-05 10:46:29 -0700

    Testing equal-path code

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,6 +8,26 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == nullptr) {
+        return true;
+    }
 
+    return (equalPaths_helper(root->left) == equalPaths_helper(root->right));
 }
 
+int equalPaths_helper (Node* parent) {
+    if (parent == nullptr) {
+        return 0;
+    }
+
+    if (parent->left && parent->right) {
+        return std::max(equalPaths_helper(parent->left), equalPaths_helper(parent->right)) + 1;
+    }
+    else if (parent->left) {
+        return equalPaths_helper(parent->left) + 1;
+    }
+    else if (parent->right) {
+        return equalPaths_helper(parent->right) + 1;
+    }
+    return 1;
+}
